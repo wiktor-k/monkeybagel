883d07570c3c1942a5f0f238f0548a9796d65b6e
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-03-22 13:03:57 +0100

    Support verify

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-use rpgpie::key::checked::CheckedCertificate;
-use std::path::PathBuf;
+use pgp::types::KeyTrait;
+use rpgpie::key::{
+    checked::CheckedCertificate,
+    component::{SignedComponentKey, SignedComponentKeyPub},
+};
+use rpgpie_cert_store::Store;
+use std::{any::Any, path::PathBuf};
 
 use clap::Parser;
 
@@ -19,6 +24,14 @@ struct Args {
 
     #[clap(long, short = 'u')]
     user_id: Option<String>,
+
+    #[clap(long)]
+    keyid_format: Option<String>,
+
+    #[clap(long)]
+    status_fd: Option<String>,
+
+    file_to_verify: Option<String>,
 }
 
 enum Mode {
@@ -57,30 +70,75 @@ impl Mode {
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    eprintln!("Args: {:#?}", args);
+    //println!("args: {:?}", args);
     let mode: Mode = args.try_into()?;
-    match &mode {
+
+    if match &mode {
         Mode::Verify(signature) => {
+            let store = Store::new().expect("FIXME");
             let mut buffer = vec![];
             std::io::copy(&mut std::io::stdin(), &mut buffer)?;
             let sig = &rpgpie::sig::load(&mut std::fs::File::open(signature)?)?[0];
-            let key: CheckedCertificate =
-                (&rpgpie::key::Certificate::load(&mut std::fs::File::open("key.asc")?)?[0]).into();
-            let valid_sigs: Vec<_> = key
-                .valid_signing_capable_component_keys_at(&std::time::SystemTime::now().into())
+            let valid_sigs = sig
+                .config
+                .issuer_fingerprint()
                 .iter()
-                .filter_map(|signing_key| signing_key.verify(sig, &buffer).map(|_| sig).ok())
-                .collect();
+                .map(|fpr| hex::encode(fpr))
+                .map(|fpr| store.search_by_fingerprint(&fpr).ok())
+                .flat_map(|certs| {
+                    certs
+                        .iter()
+                        .flatten()
+                        .flat_map(|cert| {
+                            let key: CheckedCertificate = cert.into();
+                            key.valid_signing_capable_component_keys_at(
+                                &std::time::SystemTime::now().into(),
+                            )
+                            .iter()
+                            .filter_map(|signing_key| {
+                                signing_key
+                                    .clone()
+                                    .verify(sig, &buffer)
+                                    .map(|_| (key.clone(), signing_key.clone(), sig.clone()))
+                                    .ok()
+                            })
+                            .collect::<Vec<_>>()
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+            for (key, signing_key, _sig) in &valid_sigs {
+                eprintln!(
+                    "Valid signature from certificate {}",
+                    if let SignedComponentKeyPub::Primary((primary, _)) = key.primary_key() {
+                        hex::encode(primary.fingerprint())
+                    } else {
+                        "unknown".into()
+                    }
+                );
+                eprintln!(
+                    "Valid signature from certificate {}",
+                    hex::encode(signing_key.fingerprint())
+                );
+                if let Some(user_id) = key.primary_user_id() {
+                    eprintln!("Valid signature from: {}", user_id.id.id());
+                }
+            }
+            //eprintln!("VLD: {valid_sigs:?}");
 
             if valid_sigs.is_empty() {
-                println!("no valid signatures");
+                eprintln!("No valid sigs");
+                false
             } else {
-                println!("yep, sigs valid: {valid_sigs:?}");
+                true
             }
         }
-        Mode::Sign(key) => {}
+        Mode::Sign(key) => false,
+    } {
+        mode.write_success_trailer();
+    } else {
+        std::process::exit(1);
     }
-    mode.write_success_trailer();
 
     //if email.contains('<') {
     //    email = &email[(email.find('<').unwrap() + 1)..email.find('>').unwrap()];
